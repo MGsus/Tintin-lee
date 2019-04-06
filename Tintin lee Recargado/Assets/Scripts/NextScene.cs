f8fccf75696b6508f7c3b55ee4ad81125693d7f8
Jesús Montoya <gsus.montoya.17@gmail.com>
2019-04-06 15:42:14 -0500

    Clarita First & Second Stage without animations"
    Added:
     - Images
     - Fonts
     - NextScene Script
     - First Scene of Clarita Level
     - Second Scene of Clarita Level

---
+++ Tintin lee Recargado/Assets/Scripts/NextScene.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    public string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseOver()
+    {
+        // Play Animation
+    }
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
