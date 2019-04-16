ac5475b9691c8df387dae5731546d7f47b3b082f
Jeús Montoya <gsus.montoya.17@gmail.com>
2019-04-16 15:45:14 -0500

    Button on final scene finished

--- Tintin lee Recargado/Assets/Scripts/OptionScript.cs
+++ Tintin lee Recargado/Assets/Scripts/OptionScript.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OptionScript : MonoBehaviour
 {
+    //Scene name for change by click on Olvidadiza Button
+    public string sceneName;
+    private int timer = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +19,25 @@ public class OptionScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
-        // Play shine and sound
+        // TODO Play shine and sound
     void OnMouseOver(){
     }
 
-    void OnMouseDown(){
-    
+    private void OnMouseDown(){
+        if (this.gameObject.CompareTag("Olvidadiza"))
+        {
+            // TODO Play congratulations sound
+            
+            for (timer = 0; timer < 5000; timer++)
+                print(timer);
+            
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            // Play fail sound
+            print(this.gameObject.name);
+        }
     }
 }
