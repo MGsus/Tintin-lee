750845f078be0ddc11e4f810abdc4c2e5f0abe14
Jesús Montoya <yiyismon.14@gmail.com>
2019-04-25 12:06:35 -0500

    Clarita1 Animation & 2D Outline Asset Added

---
+++ Tintin lee Recargado/Assets/Scripts/ClaritaScript.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClaritaScript : MonoBehaviour
+{
+    public string sceneName;
+    private static GameObject parentGameObject;
+    private GameObject claritaHead;
+    private GameObject claritaHeadL;
+    private int cont;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        parentGameObject = GameObject.FindGameObjectWithTag("Clarita1");
+        claritaHead = parentGameObject.transform.GetChild(0).gameObject;
+        claritaHeadL = parentGameObject.transform.GetChild(1).gameObject;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+    }
+
+    private void OnMouseOver()
+    {
+        // Animation
+        if (parentGameObject)
+        {
+            cont++;
+            parentGameObject.transform.localScale = new Vector3(1.1f, 1.1f, 1);
+            switch (cont)
+            {
+                case 60:
+                    claritaHead.SetActive(false);
+                    claritaHeadL.SetActive(true);
+                    parentGameObject.transform.Rotate(new Vector3(0,0,1), -3f);
+                    break;
+                case 120:
+                    claritaHead.SetActive(true);
+                    claritaHeadL.SetActive(false);
+                    parentGameObject.transform.Rotate(new Vector3(0,0,-1),-3f);
+                    cont = 0;
+                    break;
+            }
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (parentGameObject)
+        {
+            claritaHead.SetActive(true);
+            claritaHeadL.SetActive(false);
+            parentGameObject.transform.localScale = new Vector3(1, 1, 1);
+            parentGameObject.transform.rotation = new Quaternion(0,0,0,0);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Tintin lee Recargado/Assets/Scripts/SunScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunScript : MonoBehaviour
+{
+    private int _cont = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.Rotate(Vector3.forward, Time.deltaTime * 30f);
+        print(_cont);
+        if (_cont > 60)
+        {
+            this.transform.localScale = new Vector3(1.1f, 1.1f);
+            if (_cont == 120)
+                _cont = 0;
+        }
+        else
+        {
+            _cont++;
+            this.transform.localScale = new Vector3(1f, 1f);
+        }
+    }
+}
