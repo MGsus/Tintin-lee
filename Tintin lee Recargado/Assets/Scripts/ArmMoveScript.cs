a10efd437b442e17cb76dd0c39dfccef151361fc
Jesús Montoya <gsus.montoya.17@gmail.com>
2019-05-04 15:20:00 -0500

    Clarita4: some animations added and armMoveScript fixed

--- Tintin lee Recargado/Assets/Scripts/ArmMoveScript.cs
+++ Tintin lee Recargado/Assets/Scripts/ArmMoveScript.cs
@@ -26,40 +26,109 @@ public class ArmMoveScript : MonoBehaviour
             switch (_cont)
             {
                 case 95:
-                    brazo3.position = new Vector3(1.427f, 0.167f);
+                    brazo3.position = new Vector2(1.427f, 0.167f);
                     break;
                 case 100:
-                    brazo3.position = new Vector3(1.4f, 0.167f);
+                    brazo3.position = new Vector2(1.4f, 0.167f);
                     break;
                 case 105:
-                    brazo3.position = new Vector3(1.3f, 0.167f);
+                    brazo3.position = new Vector2(1.3f, 0.167f);
                     break;
                 case 110:
-                    brazo3.position = new Vector3(1.2f, 0.167f);
+                    brazo3.position = new Vector2(1.2f, 0.167f);
                     break;
                 case 115:
-                    brazo3.position = new Vector3(1.18f, 0.167f);
+                    brazo3.position = new Vector2(1.18f, 0.167f);
                     break;
                 case 120: // Middle Position
-                    brazo3.position = new Vector3(1.18f, 0.167f);
+                    brazo3.position = new Vector2(1.18f, 0.167f);
                     break;
                 case 125:
-                    brazo3.position = new Vector3(1.2f, 0.167f);
+                    brazo3.position = new Vector2(1.2f, 0.167f);
                     break;
                 case 130:
-                    brazo3.position = new Vector3(1.25f, 0.167f);
+                    brazo3.position = new Vector2(1.25f, 0.167f);
                     break;
                 case 135:
-                    brazo3.position = new Vector3(1.3f, 0.167f);
+                    brazo3.position = new Vector2(1.3f, 0.167f);
                     break;
                 case 140:
-                    brazo3.position = new Vector3(1.4f, 0.167f);
+                    brazo3.position = new Vector2(1.4f, 0.167f);
                     break;
                 case 145:
-                    brazo3.position = new Vector3(1.427f, 0.167f);
+                    brazo3.position = new Vector2(1.427f, 0.167f);
                     _cont = 0;
                     break;
             }
+        }else if (gameObject.name == "ClaritaArm")
+        {
+            switch (_cont)
+            {
+                case 95:
+                    brazo2.position = new Vector2(0.2f, 0.05f); // 0.2x, 0.05y
+                    break;
+                case 100:
+                    brazo2.position = new Vector2(0.17f, 0.0427f);
+                    break;
+                case 105:
+                    brazo2.position = new Vector2(0.14f, 0.0354f);
+                    break;
+                case 110:
+                    brazo2.position = new Vector2(0.11f, 0.0281f);
+                    break;
+                case 115:
+                    brazo2.position = new Vector2(0.08f, 0.0208f);
+                    break;
+                case 120: // Middle Position
+                    brazo2.position = new Vector2(0f, 0.006f); // 0x, 0.006y
+                    break;
+                case 125:
+                    brazo2.position = new Vector2(0.08f, 0.0208f);
+                    break;
+                case 130:
+                    brazo2.position = new Vector2(0.11f, 0.0281f);
+                    break;
+                case 135:
+                    brazo2.position = new Vector2(0.14f, 0.0354f);
+                    break;
+                case 140:
+                    brazo2.position = new Vector2(0.17f, 0.0427f);
+                    break;
+                case 145:
+                    brazo2.position = new Vector2(0.2f, 0.05f);
+                    break;
+                case 150:
+                    brazo2.position = new Vector2(0.17f, 0.0427f);
+                    break;
+                case 155:
+                    brazo2.position = new Vector2(0.14f, 0.0354f);
+                    break;
+                case 160:
+                    brazo2.position = new Vector2(0.11f, 0.0281f);
+                    break;
+                case 165:
+                    brazo2.position = new Vector2(0.08f, 0.0208f);
+                    break;
+                case 170: // Middle Position
+                    brazo2.position = new Vector2(0f, 0.006f); // 0x, 0.006y
+                    break;
+                case 175:
+                    brazo2.position = new Vector2(0.08f, 0.0208f);
+                    break;
+                case 180:
+                    brazo2.position = new Vector2(0.11f, 0.0281f);
+                    break;
+                case 185:
+                    brazo2.position = new Vector2(0.14f, 0.0354f);
+                    break;
+                case 190:
+                    brazo2.position = new Vector2(0.17f, 0.0427f);
+                    break;
+                case 195:
+                    brazo2.position = new Vector2(0.2f, 0.05f);
+                    _cont = 0;
+                    break;
+            }   
         }
         else
         {
--- Tintin lee Recargado/Assets/Scripts/SapoScript.cs
+++ Tintin lee Recargado/Assets/Scripts/SapoScript.cs
@@ -2,7 +2,7 @@
 
 public class SapoScript : MonoBehaviour
 {
-    private static GameObject _parentGameObject;
+    private static GameObject _sapo;
     private GameObject _sapoBody;
     private GameObject _sapoHand;
     private int _cont;
@@ -10,19 +10,70 @@ public class SapoScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _parentGameObject = GameObject.FindGameObjectWithTag("Sapo");
-        _sapoBody = _parentGameObject.transform.GetChild(0).gameObject;
-        _sapoHand = _parentGameObject.transform.GetChild(1).gameObject;
+        _sapo = GameObject.FindGameObjectWithTag("Sapo");
+        _sapoBody = _sapo.transform.GetChild(0).gameObject;
+        _sapoHand = _sapo.transform.GetChild(1).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!_parentGameObject) return;
+        if (!_sapo) return;
         switch (_cont)
         {
-            case 1:
-                _sapoBody.transform.localScale = new Vector3();
+            // Movement Animation
+            case 1: // Initial Pos
+                _sapo.transform.localScale = Vector3.one;
+                _sapo.transform.position = new Vector2(-4.03f, -0.51f);
+                break;
+            case 5:
+                
+                break;
+            case 10:
+                break;
+            case 15:
+                break;
+            case 20:
+                break;
+            case 25:
+
+                break;
+            case 30:
+
+                break;
+            case 35:
+
+                break;
+            case 40:
+
+                break;
+            case 45:
+
+                break;
+            case 50:
+
+                break;
+            case 55:
+
+                break;
+
+            // Hand Animation
+            case 60:
+                break;
+            case 65:
+                break;
+            case 70:
+                break;
+            case 75:
+                break;
+            case 80:
+                break;
+
+            // Idle Animation
+            case 230:
+                _sapo.transform.position = new Vector2(-3.614f, -0.854f);
+                _sapo.transform.localScale = Vector2.one; // Max Scale
+                _cont = 0;
                 break;
         }
     }
