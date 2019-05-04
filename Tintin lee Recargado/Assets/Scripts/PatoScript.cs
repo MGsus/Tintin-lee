3618b6df154c9899e3b7e649b338acfd45012b07
Jesús Montoya <gsus.montoya.17@gmail.com>
2019-05-04 13:45:31 -0500

    Clarita3 Animations Finished

---
+++ Tintin lee Recargado/Assets/Scripts/PatoScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatoScript : MonoBehaviour
+{
+    private static GameObject parentGameObject;
+    private GameObject patoLfHead;
+    private GameObject patoRfHead;
+    private int cont;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentGameObject = GameObject.FindGameObjectWithTag("Pato");
+        patoLfHead = parentGameObject.transform.GetChild(1).gameObject;
+        patoRfHead = parentGameObject.transform.GetChild(2).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!parentGameObject) return;
+        cont++;
+        switch (cont)
+        {
+            case 100:
+                patoLfHead.SetActive(true);
+                patoRfHead.SetActive(false);
+                break;
+            case 110:
+                patoLfHead.SetActive(false);
+                patoRfHead.SetActive(true);
+                break;
+            case 120:
+                patoLfHead.SetActive(true);
+                patoRfHead.SetActive(false);
+                break;
+            case 130:
+                patoLfHead.SetActive(false);
+                patoRfHead.SetActive(true);
+                cont = 0;
+                break;
+        }
+    }
+}
---
+++ Tintin lee Recargado/Assets/Scripts/SapoScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class SapoScript : MonoBehaviour
+{
+    private static GameObject _parentGameObject;
+    private GameObject _sapoBody;
+    private GameObject _sapoHand;
+    private int _cont;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _parentGameObject = GameObject.FindGameObjectWithTag("Sapo");
+        _sapoBody = _parentGameObject.transform.GetChild(0).gameObject;
+        _sapoHand = _parentGameObject.transform.GetChild(1).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_parentGameObject) return;
+        switch (_cont)
+        {
+            case 1:
+                _sapoBody.transform.localScale = new Vector3();
+                break;
+        }
+    }
+}
