4a5c07855375deacddfe634a0b943488cec5a5b9
Jesús Montoya <gsus.montoya.17@gmail.com>
2019-05-06 21:52:58 -0500

    Clarita6: outline and scale added

--- Tintin lee Recargado/Assets/Scripts/OptionScript.cs
+++ Tintin lee Recargado/Assets/Scripts/OptionScript.cs
@@ -9,35 +9,42 @@ public class OptionScript : MonoBehaviour
     //Scene name for change by click on Olvidadiza Button
     public string sceneName;
     private int timer = 0;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
     }
-        // TODO Play shine and sound
-    void OnMouseOver(){
+
+    void OnMouseOver()
+    {
+        transform.localScale = new Vector3(1.25f, 1.25f, 1.25f);
     }
 
-    private void OnMouseDown(){
-        if (this.gameObject.CompareTag("Olvidadiza"))
+    private void OnMouseExit()
+    {
+        transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameObject.CompareTag("Olvidadiza"))
         {
             // TODO Play congratulations sound
-            
+
             for (timer = 0; timer < 5000; timer++)
                 print(timer);
-            
+
             SceneManager.LoadScene(sceneName);
         }
         else
         {
             // Play fail sound
-            print(this.gameObject.name);
+            print(gameObject.name);
         }
     }
 }
