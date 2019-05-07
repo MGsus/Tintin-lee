3ec7b739d3aad0b4e42d147a420f9e6e952555c4
Jesús Montoya <gsus.montoya.17@gmail.com>
2019-05-07 17:26:39 -0500

    Script folder structure and clarita 7 some animations added

---
+++ Tintin lee Recargado/Assets/Scripts/Clarita7/Clarita7Script.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clarita7Script : MonoBehaviour
+{
+    private int _cont;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cont = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _cont++;
+        switch (_cont)
+        {
+            case 10:
+                transform.Rotate(new Vector3(0, 0, 1), -5f);
+                break;
+            case 15:
+                transform.Rotate(new Vector3(0, 0, -1), -2.5f);
+                break;
+            case 20:
+                transform.Rotate(new Vector3(0, 0, -1), -2.5f);
+                break;
+            case 25:
+                transform.Rotate(new Vector3(0, 0, -1), -2.5f);
+                break;
+            case 30:
+                transform.Rotate(new Vector3(0, 0, -1), -5f);
+                break;
+            case 35:
+                transform.Rotate(new Vector3(0, 0, 1), -2.5f);
+                break;
+            case 40:
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+                _cont = 0;
+                break;
+        }
+    }
+}
