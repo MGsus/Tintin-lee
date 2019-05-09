99664fd8afd7801e44da7f277a2389478bbb2bc9
Jesús Montoya <gsus.montoya.17@gmail.com>
2019-05-08 21:58:56 -0500

    Clarita 7: some animations added

--- Tintin lee Recargado/Assets/Scripts/Clarita1/ClaritaLevelScrpt.cs
+++ Tintin lee Recargado/Assets/Scripts/Clarita1/ClaritaLevelScrpt.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ClaritaLevelScrpt : MonoBehaviour
 {
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,10 @@ public class ClaritaLevelScrpt : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("Clarita1");
     }
 }
--- Tintin lee Recargado/Assets/Scripts/MainMenu/TintinScript.cs
+++ Tintin lee Recargado/Assets/Scripts/MainMenu/TintinScript.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.TerrainAPI;
+using UnityEngine.Serialization;
 
 public class TintinScript : MonoBehaviour
 {
-    private int _count;
     private static GameObject _tintin;
     private GameObject _tintinLArm;
     private GameObject _tintinRArm;
     private GameObject _tintinRFoot;
     private GameObject _tintinLFoot;
     private GameObject _balloons;
-
+    private GameObject _greenB;
+    private GameObject _blueB;
+    private GameObject _redB;
+    private GameObject _fluoB;
+    private GameObject _purpleB;
+    internal int _count;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +30,12 @@ public class TintinScript : MonoBehaviour
         _tintinRFoot = _tintin.transform.GetChild(3).gameObject;
         _tintinLFoot = _tintin.transform.GetChild(4).gameObject;
         _balloons = _tintin.transform.GetChild(5).gameObject;
+
+        _greenB = _balloons.transform.GetChild(0).gameObject;
+        _blueB = _balloons.transform.GetChild(1).gameObject;
+        _redB = _balloons.transform.GetChild(2).gameObject;
+        _fluoB = _balloons.transform.GetChild(3).gameObject;
+        _purpleB = _balloons.transform.GetChild(4).gameObject;
     }
 
     // Update is called once per frame
@@ -134,11 +147,9 @@ public class TintinScript : MonoBehaviour
             // Leave the balloons fly free!
             case 150:
                 _tintinRArm.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 15));
-//                _balloons.transform.position = new Vector2(-0.5f, 0.7f);
                 break;
             case 155:
                 _tintinRArm.transform.Rotate(new Vector3(0, 0, 1), 2.5f);
-                _balloons.transform.position = new Vector3();
                 break;
             case 160:
                 _tintinRArm.transform.Rotate(new Vector3(0, 0, 1), 2.5f);
@@ -153,8 +164,100 @@ public class TintinScript : MonoBehaviour
                 _tintinRArm.transform.Rotate(new Vector3(0, 0, 1), 2.5f);
                 break;
             case 180:
-//                _balloons.transform.position = new Vector2(-0.5f,0.8f);
                 _tintinRArm.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 30));
+                break;
+            /********** Balloon Animations ***********
+             *
+             * Initial positions for each balloon:
+             *     GreenB = x: -0.45f, y: 0.81f
+             *     BlueB = x: -0.45f, y: 0,78f
+             *     RedB = x: -0.44f, y: 0.73f
+             *     FluoB  = x: -0.55f, y: 0.76f
+             *     purpleB = x: -0.5f, y: 0.7f
+             * 
+             ****************************************/
+
+            case 185:
+                _greenB.transform.position = new Vector2(-0.45f, 0.81f);
+                _blueB.transform.position = new Vector2(-0.45f, 0.78f);
+                _redB.transform.position = new Vector2(-0.44f, 0.73f);
+                _fluoB.transform.position = new Vector2(-0.55f, 0.76f);
+                _purpleB.transform.position = new Vector2(-0.5f, 0.7f);
+                break;
+            case 190:
+                _greenB.transform.position = new Vector2(-0.456f, 0.8785f);
+                _blueB.transform.position = new Vector2(-0.3822f, 0.78f);
+                _redB.transform.position = new Vector2(-0.44f, 0.73f);
+                _fluoB.transform.position = new Vector2(-0.55f, 0.76f);
+                _purpleB.transform.position = new Vector2(0.45359f, 0.7f);
+                break;
+            case 195:
+                _greenB.transform.position = new Vector3(-0.652f, 0.947f);
+                _blueB.transform.position = new Vector3(-0.3144f, 0.78f);
+                break;
+            case 200:
+                _greenB.transform.position = new Vector3(-0.848f, 1.0155f);
+                _blueB.transform.position = new Vector3(-0.2466f, 0.78f);
+                break;
+            case 205:
+                _greenB.transform.position = new Vector3(-1.004f, 1.084f);
+                _blueB.transform.position = new Vector3(-0.1788f, 0.78f);
+                break;
+            case 210:
+                _greenB.transform.position = new Vector3(-1.24f, 1.1525f);
+                _blueB.transform.position = new Vector3(-0.111f, 0.78f);
+                break;
+            case 215:
+                _greenB.transform.position = new Vector3(-1.436f, 1.221f);
+                _blueB.transform.position = new Vector3(-0.0432f, 0.78f);
+                break;
+            case 220:
+                _greenB.transform.position = new Vector3(-1.632f, 1.2895f);
+                _blueB.transform.position = new Vector3(0.0246f, 0.78f);
+                break;
+            case 225:
+                _greenB.transform.position = new Vector3(-1.828f, 1.358f);
+                _blueB.transform.position = new Vector3(0.0924f, 0.78f);
+                break;
+            case 230:
+                _greenB.transform.position = new Vector3(-2.024f, 1.4265f);
+                _blueB.transform.position = new Vector3(0.1602f, 0.78f);
+                break;
+            case 235:
+                _greenB.transform.position = new Vector3(-2.22f, 1.495f);
+                _blueB.transform.position = new Vector3(0.228f, 0.78f);
+                break;
+            case 240:
+                _greenB.transform.position = new Vector3(-2.416f, 1.5635f);
+                _blueB.transform.position = new Vector3(0.2958f, 0.78f);
+                break;
+            case 245:
+                _greenB.transform.position = new Vector3(-2.612f, 1.632f);
+                _blueB.transform.position = new Vector3(0.3636f, 0.78f);
+                break;
+            case 250:
+                _greenB.transform.position = new Vector3(-2.808f, 1.7005f);
+                _blueB.transform.position = new Vector3(0.4314f, 0.78f);
+                break;
+            case 255:
+                _greenB.transform.position = new Vector3(-3.004f, 1.769f);
+                _blueB.transform.position = new Vector3(0.4992f, 0.78f);
+                break;
+
+            /* Final positions per balloon
+             *
+             * GreenB = x: -3.2, y: 1.77
+             * BlueB = x: 0.5, y: 1.75
+             * RedB = x: -1.2, y: 1.8
+             * FluoB = x: 1.4, y: 1.93
+             * PurpleB = x: -4.2, y: 1.97
+             */
+            case 260: // Final pos 
+                _greenB.transform.position = new Vector3(-3.2f, 1.77f);
+                _blueB.transform.position = new Vector3(0.5f, 1.75f);
+                _redB.transform.position = new Vector3(-1.2f, 1.8f);
+                _fluoB.transform.position = new Vector3(1.4f, 1.93f);
+                _purpleB.transform.position = new Vector3(-4.2f, 1.97f);
                 break;
         }
     }
